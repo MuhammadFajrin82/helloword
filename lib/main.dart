d1417587ae2341a6c4e42f4ab23a301d203ab986
AryaKalili <aryakalili221@gmail.com>
2022-03-16 01:05:38 +0800

    Berhasil

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text(
+      'Halo Dunia!-Hello Word',
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
